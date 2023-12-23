08776ed611e29d829a7dd4e02b077bf746150b23
jeevanandham-381 <jeevanandham381@gmail.com>
2023-12-23 20:02:15 +0530

    CollectorDet data added

---
+++ Data/CollectorDet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data;
+
+public class CollectorDet
+{
+    public int Id { get; set; }
+    public string DistrictName { get; set; }
+    public string DistrictCode { get; set;}
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Phone1 { get; set; }
+    public string Phone2 { get; set; }
+    public string Fax { get; set; }
+    public string Street { get; set; }
+    public string Zipcode { get; set; }
+    public string CreateedBy { get; set; }
+    public DateTime CreateData { get; set; }
+    public DateTime ModifieddData { get; set; }
+
+}
